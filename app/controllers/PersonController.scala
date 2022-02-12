434c3660f60a984433712ef212e7b5b652e58758
bastien chanez <bastien.chanez@gmail.com>
2022-02-12 19:06:34 +0100

    maj

--- app/controllers/PersonController.scala
+++ app/controllers/PersonController.scala
@@ -1,9 +1,30 @@
 package controllers
 
-class PersonController {
+import javax.inject._
+
+import models._
+import play.api.mvc._
+
+import scala.concurrent.ExecutionContext
+
+class PersonController @Inject()(repo: PersonRepository,
+                                  cc: MessagesControllerComponents
+                                )(implicit ec: ExecutionContext)
+  extends MessagesAbstractController(cc) {
+
+  val personForm: Form[CreatePersonForm] = Form {
+    mapping(
+      "firstName" -> nonEmptyText,
+      "lastName" -> nonEmptyText,
+      "age" -> number.verifying(min(1), max(150))
+    )(CreatePersonForm.apply)(CreatePersonForm.unapply)
+  }
+
+  def index = Action { implicit request =>
+    Ok(views.html.index(personForm))
+  }
 
   def getPersonList = Action.async { implicit request =>
-
   }
 
   def getPerson = Action.async { implicit request =>
--- app/models/Person.scala
+++ app/models/Person.scala
@@ -1,6 +1,6 @@
 package models
 
-case class Person(id: Long, firstName:String, lastName:String, agd:int);
+case class Person(id: Long, firstName:String, lastName:String, age:Integer);
 
 object Person {
   implicit val personFormat: OFormat[Person] = Json.format[Person]
